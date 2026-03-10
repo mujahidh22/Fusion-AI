51a809282532004f1f55686c9761ce08669dc080
mujahidh22 <mujahid442@gmail.com>
2026-03-10 16:10:43 +0530

    Installed shadcn and dark mode

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+"use client"
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useTheme } from "next-themes";
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
   return (
-   <div>
-    <h1>Fusion AI</h1>
-   </div>
+    <div>
+      <h1>Fusion AI</h1>
+      <Button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>Toggle Theme</Button>
+    </div>
   );
 }
