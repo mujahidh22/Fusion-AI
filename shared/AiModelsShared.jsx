10d7675dd39e1b6683e575f7d6cd13ebd6fd71c1
mujahidh22 <mujahid442@gmail.com>
2026-03-19 14:53:01 +0530

    setup kravix studio

--- shared/AiModelsShared.jsx
+++ shared/AiModelsShared.jsx
@@ -2,7 +2,7 @@ export const DefaultModel = {
   "GPT": { modelId: "gpt-4.1-mini" },
   "Gemini": { modelId: "gemini-2.5-flash-lite" },
   "Deepseek": { modelId: "DeepSeek-R1" },
-  "Mistral": { modelId: "mistral-medium-2505" },
+  "Mistral": { modelId: "Ministral-3b" },
   "Grok": { modelId: "grok-3-mini" },
   "Cohere": { modelId: "cohere-command-a" },
   "Llama": { modelId: "Llama-3.3-70B-Instruct" }
