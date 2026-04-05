619497e0ae48083d60334b4512c072ec2aaa7da2
mujahidh22 <mujahid442@gmail.com>
2026-04-05 17:34:00 +0530

    added suspense

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 "use client"
+import { Suspense } from "react"
 import { ChatInputBox } from "./_components/ChatInputBox"
 
 export default function Home() {
   return (
     <div>
-      <ChatInputBox />
+      <Suspense fallback={null}>
+        <ChatInputBox />
+      </Suspense>
     </div>
   );
 }
